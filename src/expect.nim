f283279add588a81c80f1dae9a228897833e787c
penguinite <penguinite@tuta.io>
2024-05-17 10:38:37 +0200

    feat: Document and slightly improve this code

--- src/expect.nim
+++ src/expect.nim
@@ -1,26 +1,71 @@
-from std/options import Option, isNone, get, some, none
+## This module implements a bunch of Rust-style `expect` procedures that you can use to get slightly cleaner code when dealing with 
+## `Option` objects.
+## 
+## Basically, this module contains 2 procedures, that basically do the same thing.
+## * The first one, when given a null `Option` result will print an error message and quit
+## * The second one when given a null `Option` will instead call the provided procedure and do nothing else.
+## 
+## Here is an example of how to use these
+runnableExamples:
+  # Let's say we are fetching some user data
+  var
+    user_submitted_data = some("Hello World!")
+    user_didnt_submit_data = none(string)
+  
+  echo expect(user_submitted_data, "Error! User didn't submit data")
+  # This will print "Hello World!"
+
+  echo expect(user_didnt_submit_data, "Error! User didn't submit data")
+  # This will print "Error! User didn't submit data" and quit
+
+from std/options import Option, isNone, get
 from std/strutils import join
 
-{.define: expectDontQuit.}
+func expect*[T](opt: Option[T], err_str: varargs[string, `$`] = ""): T =
+  ## When given a result (or more accurately, an Option object) it will check whether or not it's empty.
+  ## And if it is then it will simply print the error message that is provided and quit with an error code of "1"
+  ## 
+  ## Here is an example
+  runnableExamples:
+    type TokenKind = enum
+      Symbol, Sign
+    
+    # Some random data
+    # We can use expect to filter out any unknown tokens when we parse it.
+    let data: seq[Option[TokenKind]] = @[
+      some(Symbol), some(Symbol), none(TokenKind),
+    ]
+    
+    for i in data:
+      # Here we use expect as a kind of safety mechanism.
+      discard i.expect("Unknown token encountered!")
 
-func expect*[T](opt: Option[T], err_str: varargs[string, `$`]): T =
   if isNone(opt):
     debugEcho(err_str.join)
-    when defined(expectDontQuit):
-      return
-    else:
+    when not defined(expectDontQuit):
       quit(1)
+  else:
+    return opt.get()
+
+func expect*[T](opt: Option[T], err_func: func): T =
+  ## This procedure does the same things that `expect()` does, but it allows you to pass a procedure instead of printing an error message and quitting.
+  ## 
+  ## You sadly can't pass data to the procedure that will be called, but you can access variables in the function scope instead.
+  ## That might serve as a suitable alternative.
+  if isNone(opt):
+    err_func()
   return opt.get()
 
 proc expect*[T](opt: Option[T], err_proc: proc): T =
+  ## This procedure does the same things that `expect()` does, but it allows you to pass a procedure instead of printing an error message and quitting.
+  ## 
+  ## You sadly can't pass data to the procedure that will be called, but you can access variables in the function scope instead.
+  ## That might serve as a suitable alternative.
   if isNone(opt):
     err_proc()
-    when defined(expectDontQuit):
-      return
-    else:
-      quit(1)
   return opt.get()
 
 proc expectProc*[T](opt: Option[T], err_proc: proc): T 
   {.deprecated: "expectProc is now simply called expect(), Rename it if you'd like to".} =
+  ## This procedure does the same things that `expect()` does, but it allows you to pass a procedure instead of just a string.
   return expect(opt, err_proc)
